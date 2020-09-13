786523d8b57dc1250c8e24bd048c2d00d5cd4f5f
kaio-giovanni <kaio.giovanni3@gmail.com>
2020-09-13 15:01:09 -0300

    Configuration files

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,54 +1,36 @@
 module.exports = {
     /* ---- Origin ----  
-     Set a list of origins to be allowed. The value can be one of the following
-    
-     Boolean: true - Allow current request origin
-     Boolean: false - Disallow all
-     String - Comma separated list of allowed origins
-     Array - An array of allowed origins
-     String: * - A wildcard to allow current request origin
-     Function - Receives the current origin and should return one of the above values.  
+     Lista de Origins que serão aceitas.
     */
     origin: [
-        "http://localhost:3333",
+        "http://localhost:3000",
         "https://agrotech.herokuapp.com"
     ],
   
     /* ---- Methods ----
-     HTTP methods to be allowed. The value can be one of the following
-    
-     String - Comma separated list of allowed methods
-     Array - An array of allowed methods  
+     Métodos HTTP que serão aceitos.
     */
     methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
   
     /* ---- Headers ----  
-     List of headers to be allowed via Access-Control-Request-Headers header.
-     The value can be one of the following.
-    
-     String - Comma separated list of allowed headers
-     Array - An array of allowed headers
-     String: * - A wildcard to allow current request headers
+     Lista de cabeçalhos que serão aceitos via Access-Control-Request-Headers.
     */
-    allowedHeaders: [],
+    allowedHeaders: [
+        "content-lenght", "content-type"
+    ],
   
     /*---- Expose Headers ----
-     A list of headers to be exposed via `Access-Control-Expose-Headers`
-     header. The value can be one of the following.
-    
-     String: Comma separated list of allowed headers
-     Array - An array of allowed headers  
+     Lista de cabeçalhos exposed via Access-Control-Expose-Headers
     */
     exposedHeaders: [],
   
     /* ---- Credentials ----
-     Define Access-Control-Allow-Credentials header. It should always be a
-     boolean.  
+     Define o cabeçalho Access-Control-Allow-Credentials.
     */
     credentials: false,
   
     /* ---- optionsSuccessStatus ----
-     Define status code success
+     Define codigo status de sucesso.
     */
     optionsSuccessStatus: 200
   }
